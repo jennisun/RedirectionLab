6e9190a9179df4ef8890c2630bc4e068befbcda8
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2021-01-02 13:36:27 -0500

    done yay

--- MakeStars.java
+++ MakeStars.java
@@ -28,18 +28,16 @@ public class MakeStars {
   }
 
 
-  public static void main(String[] args) {
-
-    // creates a Scanner object
-  Scanner input = new Scanner(System.in);
-
-
-  ArrayList<String> a = new ArrayList<String>();
-  String[] b = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
-  for (int i = 0; i < b.length; i ++) {
-    a.add(b[i]);
+  public static String pigLatinBest(String k){
+    if (!(97 <= (int)k.charAt(0) && (int)k.charAt(0) <= 122)) return(k);
+    else if (!(97 <= (int)k.charAt(k.length() - 1) && (int)k.charAt(k.length() - 1) <= 122)) return(pigLatin(k.substring(0, k.length() - 1)) + String.valueOf(k.charAt(k.length() - 1)));
+    else return(pigLatin(k));
   }
 
+
+  public static void main(String[] args) {
+    // creates a Scanner object
+  Scanner input = new Scanner(System.in);
 
   while (input.hasNextLine()) {
 
@@ -47,27 +45,12 @@ public class MakeStars {
     while (data1.hasNext()) {
       String k = data1.next();
 
-      if (k.charAt(0) == 'a' || k.charAt(0) == 'e' || k.charAt(0) == 'i' || k.charAt(0) == 'o' || k.charAt(0) == 'u') {
-        System.out.print(k + "hay");
-      }
-      else {
-        if (k.length() >= 2 && a.contains(k.substring(0, 2))) System.out.print(k.substring(2, k.length()) + k.substring(0, 2) + "ay");
-        else System.out.print(k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
-      }
-
-      // for (int i = 0; i < k.length(); i ++) {
-      //   System.out.print('*');
-      // }
-      System.out.print(' ');
+      System.out.print(pigLatinBest(k.toLowerCase()) + ' ');
     }
-
     System.out.println("");
   }
 
-
   input.close();
 
-
   }
-
 }
