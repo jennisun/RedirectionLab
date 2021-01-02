c76dbc4b7d032444f79052b502d7780292853a86
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2021-01-02 13:10:26 -0500

    fixed pig latin 1 and 2

--- MakeStars.java
+++ MakeStars.java
@@ -2,43 +2,70 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class MakeStars {
-  public static void main(String[] args) {
 
-    // creates a Scanner object
-    Scanner input = new Scanner(System.in);
+  public static String pigLatinSimple(String k){
+    if (k.charAt(0) == 'a' || k.charAt(0) == 'e' || k.charAt(0) == 'i' || k.charAt(0) == 'o' || k.charAt(0) == 'u') {
+      return(k + "hay");
+    }
+    else return (k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
+  }
 
+
+  public static String pigLatin(String k){
     ArrayList<String> a = new ArrayList<String>();
     String[] b = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
     for (int i = 0; i < b.length; i ++) {
       a.add(b[i]);
     }
 
+    if (k.charAt(0) == 'a' || k.charAt(0) == 'e' || k.charAt(0) == 'i' || k.charAt(0) == 'o' || k.charAt(0) == 'u') {
+      return (k + "hay");
+    }
+    else {
+      if (k.length() >= 2 && a.contains(k.substring(0, 2))) return(k.substring(2, k.length()) + k.substring(0, 2) + "ay");
+      else return(k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
+    }
+  }
 
-    while (input.hasNextLine()) {
 
-      Scanner data1 = new Scanner(input.nextLine());
-      while (data1.hasNext()) {
-        String k = data1.next();
+  public static void main(String[] args) {
 
-        if (k.charAt(0) == 'a' || k.charAt(0) == 'e' || k.charAt(0) == 'i' || k.charAt(0) == 'o' || k.charAt(0) == 'u') {
-          System.out.print(k + "hay");
-        }
-        else {
-          if (k.length() >= 2 && a.contains(k.substring(0, 2))) System.out.print(k.substring(2, k.length()) + k.substring(0, 2) + "ay");
-          else System.out.print(k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
-        }
+    // creates a Scanner object
+  Scanner input = new Scanner(System.in);
 
-        // for (int i = 0; i < k.length(); i ++) {
-        //   System.out.print('*');
-        // }
-        System.out.print(' ');
+
+  ArrayList<String> a = new ArrayList<String>();
+  String[] b = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+  for (int i = 0; i < b.length; i ++) {
+    a.add(b[i]);
+  }
+
+
+  while (input.hasNextLine()) {
+
+    Scanner data1 = new Scanner(input.nextLine());
+    while (data1.hasNext()) {
+      String k = data1.next();
+
+      if (k.charAt(0) == 'a' || k.charAt(0) == 'e' || k.charAt(0) == 'i' || k.charAt(0) == 'o' || k.charAt(0) == 'u') {
+        System.out.print(k + "hay");
+      }
+      else {
+        if (k.length() >= 2 && a.contains(k.substring(0, 2))) System.out.print(k.substring(2, k.length()) + k.substring(0, 2) + "ay");
+        else System.out.print(k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
       }
 
-      System.out.println("");
+      // for (int i = 0; i < k.length(); i ++) {
+      //   System.out.print('*');
+      // }
+      System.out.print(' ');
     }
 
+    System.out.println("");
+  }
 
-    input.close();
+
+  input.close();
 
 
   }
