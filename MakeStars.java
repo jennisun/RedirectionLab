9f562a12d37bcc01f82d4472fba89f58b9ef83ce
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2021-01-02 12:59:54 -0500

    pig latin 2 done

--- MakeStars.java
+++ MakeStars.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MakeStars {
   public static void main(String[] args) {
@@ -6,17 +7,14 @@ public class MakeStars {
     // creates a Scanner object
     Scanner input = new Scanner(System.in);
 
-    // reads an int value
+    ArrayList<String> a = new ArrayList<String>();
+    String[] b = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < b.length; i ++) {
+      a.add(b[i]);
+    }
+
+
     while (input.hasNextLine()) {
-      // Scanner data1 = new Scanner(input.nextLine());
-      //
-      // String k = data1.next();
-      // String output = "";
-      // for (int i = 0; i < k.length(); i ++) {
-      //   if (k.charAt(i) != ' ') output += "*";
-      //   else output += " ";
-      // }
-      // System.out.println(output);
 
       Scanner data1 = new Scanner(input.nextLine());
       while (data1.hasNext()) {
@@ -25,7 +23,10 @@ public class MakeStars {
         if (k.charAt(0) == 'a' || k.charAt(0) == 'e' || k.charAt(0) == 'i' || k.charAt(0) == 'o' || k.charAt(0) == 'u') {
           System.out.print(k + "hay");
         }
-        else System.out.print(k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
+        else {
+          if (k.length() >= 2 && a.contains(k.substring(0, 2))) System.out.print(k.substring(2, k.length()) + k.substring(0, 2) + "ay");
+          else System.out.print(k.substring(1, k.length()) + String.valueOf(k.charAt(0)) + "ay");
+        }
 
         // for (int i = 0; i < k.length(); i ++) {
         //   System.out.print('*');
